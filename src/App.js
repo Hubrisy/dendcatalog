588f4b7f8c1d64ef9bdb716799d8da231f42bce8
Hubrisy <ahtaras67@gmail.com>
2024-05-17 09:18:04 +0300

    new update

--- src/App.js
+++ src/App.js
@@ -57,12 +57,12 @@ function App() {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '423735837042917');
+          fbq('init', '965374158415930');
           fbq('track', 'PageView');
       `}
 </script>
 <noscript>
-{`<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=423735837042917&amp;ev=PageView&amp;noscript=1"/>`}
+{`<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=965374158415930&amp;ev=PageView&amp;noscript=1"/>`}
 </noscript>
     </Helmet>
 
